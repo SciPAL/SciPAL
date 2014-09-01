c914ea060cf1ef3d0bd32eb43da8225d0162baf8
JHoahg <johannes.hagemann@phys.uni-goettingen.de>
2014-09-01 23:38:36 +0200

    remove doc from master

--- doc/autogen/CUDA_HPC_Praktikum/search/all_73.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['step_2d1_2eh',['step-1.h',['../step-1_8h.html',1,'']]],
-  ['step_2d2_2eh',['step-2.h',['../step-2_8h.html',1,'']]],
-  ['step_2d42_2eh',['step-42.h',['../step-42_8h.html',1,'']]],
-  ['step_2d7_2eh',['step-7.h',['../step-7_8h.html',1,'']]]
-];
